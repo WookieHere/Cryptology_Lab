585083b758ed87831e93a6a02f1a0a1b1353ed7f
Luke Nasby <wookiehere99@gmail.com>
2026-01-29 01:32:43 +0100

    Added a section to skip spaces

--- vern_enc.py
+++ vern_enc.py
@@ -8,10 +8,12 @@ def shift_letter(input_letter, key_letter):
     return ascii_map[(index_of_letter(input_letter) + index_of_letter(key_letter)) % len(ascii_map)]
 
 def encrypt_phrase(input_phrase, input_key):
+    new_word = ""
     for i in range(0, len(input_phrase)):
-        print(shift_letter(input_phrase[i], input_key[i % len(input_key)]))
-    print(input_phrase)
+        if input_phrase[i] != ' ':
+            new_word += shift_letter(input_phrase[i], input_key[i % len(input_key)])
+    print(new_word)
 
-input_phrase = "happy"
+input_phrase = "happy to meet you"
 input_key = "no"
 encrypt_phrase(input_phrase, input_key)
